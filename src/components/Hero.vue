df9eaaa2c2f5738e129265af179443b09ac556b0
Liran Tal <liran.tal@gmail.com>
2020-09-19 17:11:46 +0300

    feat: use random splash images

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <v-row no-gutters>
       <v-img
         :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
-        src="https://images.unsplash.com/photo-1556865897-3e02fedb1774?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=933&q=80"
+        :src="randomHeroImage"
       >
         <!-- <v-theme-provider dark> -->
           <v-container class="pt-10">
@@ -50,8 +50,22 @@
 <script>
 import AuthorCard from "./AuthorCard";
 
+const heroImages = [
+  "https://images.unsplash.com/photo-1556865897-3e02fedb1774?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=933&q=80",
+  "https://images.unsplash.com/photo-1557321259-edfc29fa4053?ixlib=rb-1.2.1&auto=format&fit=crop&w=2552&q=80",
+  "https://images.unsplash.com/photo-1559294824-627e9738df81?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+  "https://images.unsplash.com/photo-1569225022281-f652746229fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+  "https://images.unsplash.com/photo-1547751550-b62e1c2e9770?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80"
+]
+const randomHeroImageIndex = Math.floor(Math.random()*heroImages.length)
+
 export default {
   name: "Hero",
-  components: { AuthorCard }
+  components: { AuthorCard },
+  data: () => {
+    return {
+      randomHeroImage: heroImages[randomHeroImageIndex]
+    }
+  }
 };
 </script>
