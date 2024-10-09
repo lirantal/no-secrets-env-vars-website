d67e3fd4af248c84d4342873ca71e7ad795c6592
Liran Tal <liran.tal@gmail.com>
2024-10-09 21:29:03 +0300

    feat: commit new secrets in env vars website re-make

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
-  // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: 'https://no-secrets-env-vars-website.vercel.app/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
